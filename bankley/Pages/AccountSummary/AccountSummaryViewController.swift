bf81979a57852abe3a3766dc4946885035642f7d
aldinugroho22 <v.aldi.nugroho@adira.co.id>
2022-11-07 16:54:12 +0700

    update

--- bankley/Pages/AccountSummary/AccountSummaryViewController.swift
+++ bankley/Pages/AccountSummary/AccountSummaryViewController.swift
@@ -32,7 +32,9 @@ extension AccountSummaryViewController {
     private func setuptableview() {
         tableView.delegate = self
         tableView.dataSource = self
-        
+        tableView.register(AccountSummaryCell.self, forCellReuseIdentifier: AccountSummaryCell.reuseID)
+        tableView.rowHeight = AccountSummaryCell.rowHeight
+        tableView.tableFooterView = UIView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
         
@@ -53,8 +55,7 @@ extension AccountSummaryViewController {
 
 extension AccountSummaryViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = games[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: AccountSummaryCell.reuseID, for: indexPath) as! AccountSummaryCell
         return cell
     }
     
