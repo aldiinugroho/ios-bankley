21ca13545a8383b3af5b3b8ebf5363ae9ddbb512
aldinugroho22 <v.aldi.nugroho@adira.co.id>
2022-11-07 16:16:07 +0700

    update

--- bankley/Pages/AccountSummary/AccountSummaryHeaderView.swift
+++ bankley/Pages/AccountSummary/AccountSummaryHeaderView.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class AccountSummaryHeaderView: UIView {
     
+    let descStackView = UIStackView()
+    let descnimageStackView = UIStackView()
+    let titleLabel = UILabel()
+    let greetingLabel = UILabel()
+    let nameLabel = UILabel()
+    let dateLabel = UILabel()
+    let imageView = UIImageView()
+    
+    let imageBoxView = UIView()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         style()
@@ -23,9 +33,53 @@ class AccountSummaryHeaderView: UIView {
 extension AccountSummaryHeaderView {
     func style() {
         self.backgroundColor = appColor
+        
+        descnimageStackView.translatesAutoresizingMaskIntoConstraints = false
+        descnimageStackView.axis = .horizontal
+        descnimageStackView.distribution = .equalSpacing
+        descnimageStackView.alignment = .leading
+//        descnimageStackView.backgroundColor = .systemPink
+        
+        descStackView.translatesAutoresizingMaskIntoConstraints = false
+        descStackView.axis = .vertical
+        descStackView.spacing = 5
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        titleLabel.text = "Bankley"
+        
+        greetingLabel.translatesAutoresizingMaskIntoConstraints = false
+        greetingLabel.text = "Good Morning,"
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.text = "Jonathan"
+        
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        dateLabel.text = "Date"
+        
+        let configuration = UIImage.SymbolConfiguration(pointSize: 60, weight: .light, scale: .default)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "sun.max.fill", withConfiguration: configuration)
+        imageView.tintColor = .systemYellow
+        imageView.contentMode = .scaleAspectFit
     }
     
     func layout() {
+        self.addSubview(descnimageStackView)
         
+        descStackView.addArrangedSubview(titleLabel)
+        descStackView.addArrangedSubview(greetingLabel)
+        descStackView.addArrangedSubview(nameLabel)
+        descStackView.addArrangedSubview(dateLabel)
+
+        descnimageStackView.addArrangedSubview(descStackView)
+        descnimageStackView.addArrangedSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            descnimageStackView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            descnimageStackView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            descnimageStackView.leadingAnchor.constraint(equalToSystemSpacingAfter: self.leadingAnchor, multiplier: 2),
+            self.trailingAnchor.constraint(equalToSystemSpacingAfter: descnimageStackView.trailingAnchor, multiplier: 2)
+        ])
     }
 }
