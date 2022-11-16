9bae1b6b079c0127d09b5141a7099c8e3299ff15
aldinugroho22 <v.aldi.nugroho@adira.co.id>
2022-11-16 10:46:43 +0700

    update

---
+++ bankleyTests/AccountSummary/ProfileTests.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileTests.swift
+//  bankleyTests
+//
+//  Created by aldinugroho on 16/11/22.
+//
+
+import Foundation
+import XCTest
+
+@testable import bankley
+
+class ProfileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func testCanParse() throws {
+        let json = """
+        {
+        "id": "1",
+        "first_name": "Kevin",
+        "last_name": "Flynn",
+        }
+        """
+        
+        let data = json.data(using: .utf8)!
+        let result = try! JSONDecoder().decode(Profile.self, from: data)
+        
+        XCTAssertEqual(result.id, "1")
+        XCTAssertEqual(result.firstName, "Kevin")
+        XCTAssertEqual(result.lastName, "Flynn")
+    }
+}
